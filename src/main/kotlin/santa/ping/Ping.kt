49cbf459fc07504dc7f8bcc9ee703164e458e3a2
elifoster <elifosterwy@gmail.com>
2015-11-07 10:22:42 -0800

    Oh, apparently I accidentally got rid of literally all the code.

--- src/main/kotlin/santa/ping/Ping.kt
+++ src/main/kotlin/santa/ping/Ping.kt
@@ -1,3 +1,12 @@
 package santa.ping
 
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.common.MinecraftForge
+
+@Mod(name = "Ping!", modid = "ping", version = "1.0.0") class Ping {
+    @Mod.EventHandler fun init(event: FMLInitializationEvent) {
+        MinecraftForge.EVENT_BUS.register(PingEventHandler())
+    }
+}
 
--- src/main/kotlin/santa/ping/PingEventHandler.kt
+++ src/main/kotlin/santa/ping/PingEventHandler.kt
@@ -1,3 +1,17 @@
 package santa.ping
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent
+import net.minecraft.client.Minecraft
+import net.minecraftforge.client.event.ClientChatReceivedEvent
 
+class PingEventHandler {
+    @SubscribeEvent fun onChatMessage(event: ClientChatReceivedEvent) {
+        var mc = Minecraft.getMinecraft()
+        var player = mc.thePlayer
+        var world = mc.theWorld
+        var text = event.message.unformattedText
+        if (text.contains(player.displayName, false)) {
+            world.playSoundAtEntity(player, "ping:ping", 1.0F, 1.0F)
+        }
+    }
+}
