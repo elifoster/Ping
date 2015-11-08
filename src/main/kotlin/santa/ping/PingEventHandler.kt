e32cafa660b15b57d56cf2c26ddbcc2b88892221
elifoster <elifosterwy@gmail.com>
2015-11-08 00:05:36 -0800

    More sounds and configuration for it. Fix matching on every message.

--- src/main/kotlin/santa/ping/PingEventHandler.kt
+++ src/main/kotlin/santa/ping/PingEventHandler.kt
@@ -1,17 +1,24 @@
 package santa.ping
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent
+import cpw.mods.fml.relauncher.Side
+import cpw.mods.fml.relauncher.SideOnly
 import net.minecraft.client.Minecraft
 import net.minecraftforge.client.event.ClientChatReceivedEvent
+import kotlin.text.Regex
 
 class PingEventHandler {
-    @SubscribeEvent fun onChatMessage(event: ClientChatReceivedEvent) {
-        var mc = Minecraft.getMinecraft()
-        var player = mc.thePlayer
-        var world = mc.theWorld
-        var text = event.message.unformattedText
-        if (text.contains(player.displayName, false)) {
-            world.playSoundAtEntity(player, "ping:ping", 1.0F, 1.0F)
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    fun onChatMessage(event: ClientChatReceivedEvent) {
+        val mc = Minecraft.getMinecraft()
+        val player = mc.thePlayer
+        val name = player.displayName.toLowerCase()
+        var text = event.message.unformattedText.toLowerCase()
+        val regex = Regex("<.+>")
+        text = text.replaceFirst(regex, "")
+        if (text.contains(name) == true) {
+            player.playSound("ping:${Config.soundType}", 1.0F, 1.0F)
         }
     }
 }
