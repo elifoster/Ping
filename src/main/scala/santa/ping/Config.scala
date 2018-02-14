ad84d5e2494a3ec6bfb58fec7657ed443b74eb4b
elifoster <elifosterwy@gmail.com>
2018-02-13 22:50:07 -0800

    :shirt: Clean up missing type annotations in Config class

--- src/main/scala/santa/ping/Config.scala
+++ src/main/scala/santa/ping/Config.scala
@@ -8,7 +8,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 class Config(event: FMLPreInitializationEvent) {
   val DEFAULT_SOUND = "blop"
   val VALID_TYPES = List("blop", "gum", "pling", "ping", "ting", "pop")
-  val COMMENT = String.format("The sound types to use as the ping noise. Can be any of the " +
+  val COMMENT: String = String.format("The sound types to use as the ping noise. Can be any of the " +
     " following: %s. Defaults to %s, and will be %s if the value here is invalid.",
     VALID_TYPES.mkString(", "), DEFAULT_SOUND, DEFAULT_SOUND)
 
@@ -16,27 +16,27 @@ class Config(event: FMLPreInitializationEvent) {
   config.load()
 
   config.addCustomCategoryComment("Sound Type", COMMENT)
-  var soundType = config.get("Sound Type", "The sound type", DEFAULT_SOUND).getString
+  var soundType: String = config.get("Sound Type", "The sound type", DEFAULT_SOUND).getString
   if (!VALID_TYPES.exists(s => s.equals(soundType))) {
     soundType = DEFAULT_SOUND
     FMLLog.warning("[Ping!] Ping sound type is invalid. Using blop. Please fix your config.")
   }
 
   config.addCustomCategoryComment("Custom Names", "Separate each with a semicolon ;")
-  val customNamesString = config.get("Custom Names", "Custom names to be pinged as", "").getString
-  val customNames = getNamesFromConfig(customNamesString)
+  val customNamesString: String = config.get("Custom Names", "Custom names to be pinged as", "").getString
+  val customNames: Option[Array[String]] = getNamesFromConfig(customNamesString)
 
   config.addCustomCategoryComment("Custom Color", "The color to highlight the text. Can be any of the " + "following: aqua, black, blue, dark aqua, dark blue, dark gray, dark grey, dark green, dark purple, " + "dark red, gold, gray, grey, green, light purple, red, yellow")
-  val customColor = getColorFromConfig(config.get("Custom Color", "The custom color for the highlighted text", "").getString)
+  val customColor: Option[TextFormatting] = getColorFromConfig(config.get("Custom Color", "The custom color for the highlighted text", "").getString)
   if (customColor.isEmpty) FMLLog.warning("[Ping!] Highlight color is invalid. Please fix your config.")
 
-  val bold = config.getBoolean("Bold", "Chat Style", true, "")
-  val italic = config.getBoolean("Italic", "Chat Style", false, "")
-  val strikethrough = config.getBoolean("Strikethrough", "Chat Style", false, "")
-  val underline = config.getBoolean("Underline", "Chat Style", false, "")
+  val bold: Boolean = config.getBoolean("Bold", "Chat Style", true, "")
+  val italic: Boolean = config.getBoolean("Italic", "Chat Style", false, "")
+  val strikethrough: Boolean = config.getBoolean("Strikethrough", "Chat Style", false, "")
+  val underline: Boolean = config.getBoolean("Underline", "Chat Style", false, "")
   config.save()
 
-  def getNamesFromConfig(base: String) = if (base.isEmpty) None else Some(base.split(";"))
+  def getNamesFromConfig(base: String): Option[Array[String]] = if (base.isEmpty) None else Some(base.split(";"))
 
   def getColorFromConfig(color: String): Option[TextFormatting] = {
     color match {
